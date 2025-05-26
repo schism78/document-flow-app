d9dd835cd1be5a448c21dbf5570f38bf8ce02223
olrug <artvik48@gmail.com>
2025-05-26 16:30:31 +0300

    feat: add connection to Yandex Cloud Object Storage

--- api/Program.cs
+++ api/Program.cs
@@ -1,20 +1,40 @@
 using api.Data;
 using Microsoft.EntityFrameworkCore;
 using DotNetEnv;
+using Amazon.S3;
+using Amazon.S3.Model;
+using YandexCloudStorageApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
 
-string dbName = Environment.GetEnvironmentVariable("DB_NAME");
-string dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
-string dbPass = Environment.GetEnvironmentVariable("DB_PASSWORD");
+string? dbName = Environment.GetEnvironmentVariable("DB_NAME");
+string? dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
+string? dbPass = Environment.GetEnvironmentVariable("DB_PASSWORD");
 
 string connectionString = $"Host=localhost;Port=5432;Database={dbName};Username={dbUser};Password={dbPass}";
 
 // Регистрируем DbContext с PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
+
+builder.Services.AddSingleton<YandexStorageService>();
+builder.Services.AddControllers();
+
+string? serviceUrl = Environment.GetEnvironmentVariable("SERVICE_URL");
+string? accessKey = Environment.GetEnvironmentVariable("ACSESS_KEY");
+string? secretKey = Environment.GetEnvironmentVariable("SECRET_KEY");
+
+builder.Services.AddSingleton<IAmazonS3>(sp =>
+{
+    var s3Config = new AmazonS3Config
+    {
+        ServiceURL = serviceUrl, // Указываем URL Yandex Object Storage
+        ForcePathStyle = true    // Требуется для совместимости с Yandex Cloud
+    };
+    return new AmazonS3Client(accessKey, secretKey, s3Config);
+});
 
 var app = builder.Build();
 
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4495a9a8e2a6b36b568c46a187273390bffcd75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4f568f380096f4de62fc7020c22493f8540cc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
