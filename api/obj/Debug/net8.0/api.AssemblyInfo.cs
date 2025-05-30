528124780df13481c69f482ae6ed93a49e2c2dec
schism78 <artvik50@gmail.com>
2025-05-30 13:04:09 +0300

    feat: add create file button

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5e05c9dd6564041285770a60bf713b7091960e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f23ec12f9ccf13472937576803f21c0c867e85c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
