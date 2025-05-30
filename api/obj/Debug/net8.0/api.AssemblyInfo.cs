2f23ec12f9ccf13472937576803f21c0c867e85c
schism78 <artvik50@gmail.com>
2025-05-30 12:50:58 +0300

    refactor: rebuild migrations

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd832e034130f4fd712e516688640e559ee5ff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5e05c9dd6564041285770a60bf713b7091960e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
